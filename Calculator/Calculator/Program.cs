0d61bbf89ce5fc124b84a66d12a5f9f2a8e8a20d
Anastasia <girl-physicist@mail.ru>
2023-09-26 16:57:25 +0300

    recomend.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -3,18 +3,31 @@ using System.Runtime.InteropServices;
 
 internal class Program
 {
+        double result;
+        char a; // имя должно передавать смысл переменной. например typeOfTransaction
+        char again;
+    // метод Main() не должен содержать функционала, в нем следует лишь вызывать нужные вам методы (а вот в них уже функционал)
+    // общедуступные переменные следует объявлять как поля класса, а не переменные метода Main()
     private static void Main(string[] args)
     {
-        double result;
-        char a;
-        char again;
-        link1:
-        Console.WriteLine("Введите первое число: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Введите оператор(+,-,*,/): ");
-        a = Convert.ToChar(Console.ReadLine());
-        Console.WriteLine("Введите второе число: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+       
+        link1: // в программе не должны еспользоваться якоря и оператор goto (говорила об этом на лекции)
+        GetInputData ();
+        Calc ();
+        RepeatProgram();
+        
+    }
+    static void RepeatProgram()
+    {
+        Console.WriteLine("Желаете продолжить?(y/n)");
+        again = Convert.ToChar(Console.ReadLine()); // обработка исключения
+        if (again == 'y') goto link1; // убрать якоря
+        else goto link2;
+        link2:
+        Console.WriteLine("Удачи!");
+    }
+    static void Calc ()
+    {
         switch(a)
         {
             case '+':
@@ -37,11 +50,14 @@ internal class Program
                 Console.WriteLine("Неизвестный оператор");
                 break;
         }
-        Console.WriteLine("Желаете продолжить?(y/n)");
-        again = Convert.ToChar(Console.ReadLine());
-        if (again == 'y') goto link1; 
-        else goto link2;
-        link2:
-        Console.WriteLine("Удачи!");
+    }
+    static void GetInputData ()
+    {
+        Console.WriteLine("Введите первое число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
+        Console.WriteLine("Введите оператор(+,-,*,/): ");
+        a = Convert.ToChar(Console.ReadLine()); // обработка исключения
+        Console.WriteLine("Введите второе число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine()); // оюработка исключения
     }
 }
